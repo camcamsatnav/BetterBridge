5161071888608c8c17b0a026a0d9972ee15e0bfa
Camden Jones <cj00642@surrey.ac.uk>
2024-05-03 19:58:48 +0100

    refactored

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import Settings from "./settings";
 
 register("command", () => {
     Settings.openGUI();
-}).setTabCompletions('settings').setName('bridge')
+}).setName('bridge')
 
 
 const colorArray = ["&4", "&c", "&6", "&e", "&2", "&a", "&b", "&3", "&1", "&9", "&d", "&5", "&f", "&7", "&8", "&0"];
@@ -14,20 +14,17 @@ register("chat", bridgeChat).setCriteria("&r&2Guild > ${bot}: &r${player}" + Set
 function bridgeChat(bot, player, msg, event) {
     if (!Settings.enabled || Settings.botName.toLowerCase() !== ChatLib.removeFormatting(bot).toLowerCase()) return;
     cancel(event);
-    bridgeText = Settings.prefix;
-    if (Settings.prefixBold === true) { bridgeBold = "&l" } else { bridgeBold = "" }
-    if (Settings.userBold === true) { discordBold = "&l" } else { discordBold = "" }
-    bridgeColor = colorArray[Settings.bridgeColor]
-    discordColor = colorArray[Settings.userColor]
-    let combined = addLinks(`§2Guild > ${bridgeColor + bridgeBold + bridgeText} ${discordColor + discordBold + player}§r: ${msg}`, event)
+    let bridgeBold = Settings.prefixBold ? "&l" : "";
+    let discordBold = Settings.userBold ? "&l" : "";
+    let combined = addLinks(`§2Guild > ${colorArray[Settings.bridgeColor] + bridgeBold + Settings.prefix} ${colorArray[Settings.userColor] + discordBold + player}§r: ${msg}`, event);
     ChatLib.chat(combined);
 }
 
 function addLinks(msg, event) { //adds the links and hoverables to the output string
     let newmsg = new TextComponent(msg.replace("&rhttp", "http"));
     new Message(event).getMessageParts().forEach(part => {
-        if (part.clickValue) newmsg.setClick(part.clickAction, part.clickValue)
-        if (part.hoverValue) newmsg.setHoverValue(part.hoverValue)
-    })
-    return newmsg
+        if (part.clickValue) newmsg.setClick(part.clickAction, part.clickValue);
+        if (part.hoverValue) newmsg.setHoverValue(part.hoverValue);
+    });
+    return newmsg;
 }
